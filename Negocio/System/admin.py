5d0c2abf755fb78d31b8d4bbe9735aabd9fb76c3
MaximoSav <maximosavino10@gmail.com>
2020-07-01 20:04:38 -0300

    Clases

---
+++ Negocio/System/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from System.models import *
+
+# Register your models here.
+
+admin.site.register(Direccion)
+admin.site.register(Cliente)
+admin.site.register(Venta)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
---
+++ Negocio/System/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+
+class Direccion(models.Model):
+    calle = models.CharField(max_length = 50)
+    numero = models.IntegerField()
+    comuna = models.CharField(max_length = 50)
+    ciudad = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return ("{}:{}".format(self.calle, self.numero, self.comuna, self.ciudad))
+
+class Cliente(models.Model):
+    nombre =  models.CharField(max_length = 50)
+    RUT = models.IntegerField(primary_key=True)
+    telefono = models.IntegerField()
+    direccion = models.ForeignKey('Direccion', on_delete = models.CASCADE,)
+
+    def __str__(self):
+        return ("{}:{}".format(self.nombre, self.RUT, self.telefono, self.direccion))
+
+class Venta(models.Model):
+    RUT = models.IntegerField(primary_key=True)
+    fecha = models.DateField() 
+    monto_final = models.IntegerField()
+    descuento = models.IntegerField()
+
+    def __str__(self):
+        return ("{}:{}".format(self.RUT, self.fecha, self.monto_final, self.descuento))
+
+class Producto(models.Model):
+    RUT = models.IntegerField(primary_key=True)
+    nombre =  models.CharField(max_length = 50)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return ("{}:{}".format(self.RUT, self.nombre, self.precio, self.stock))
+
+class Categoria(models.Model):
+    RUT = models.IntegerField(primary_key=True)
+    nombre =  models.CharField(max_length = 50)
+    descripcion =  models.CharField(max_length = 80)
+
+    def __str__(self):
+        return ("{}:{}".format(self.RUT, self.nombre, self.descripcion))
+
+class Proveedor(models.Model):
+    web = models.CharField(max_length = 50)
+    nombre =  models.CharField(max_length = 50)
+    RUT = models.IntegerField(primary_key=True)
+    direccion = models.ForeignKey('Direccion', on_delete = models.CASCADE,)
+
+    def __str__(self):
+        return ("{}:{}".format(self.RUT, self.nombre, self.web, self.direccion))
+
+
